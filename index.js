a99419863fb171399cae13f6b99c5ed37cbfcb98
noemi.garcia <noemiltgarcia@gmail.com>
2023-04-12 12:53:30 -0500

    Update index.js
    Created navbar clickable links using .js function

---
+++ index.js
@@ -0,0 +1,19 @@
+// ==================== Auto navbar scrolling ==================== 
+const section=document.querySelector('section')
+const navlink=document.querySelector('header nav a')
+
+window.onscroll=()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = set.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navlink.forEach(links=>{
+                links.classlis.remove('active')
+                document.querySelector('header nav a[href *= '+id+']').classList.add('active')
+            })
+        }
+    })
+}
